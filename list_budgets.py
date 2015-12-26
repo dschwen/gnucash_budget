698f59f50a4c66c9b343cbb55b1278f5b9f11f0d
Daniel Schwen <daniel@schwen.de>
2015-12-25 17:15:41 -0700

    Initial commit

---
+++ list_budgets.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import _mysql
+import sys
+
+# user needs to provide database info and credentials here
+import config
+
+query = 'select guid, name from budgets;'
+
+try:
+    con = _mysql.connect(host=config.host, port=config.port,
+                         user=config.user, passwd=config.password,
+                         db=config.database)
+    con.query(query)
+    result = con.use_result()
+
+    print result.fetch_row()[0]
+
+except _mysql.Error, e:
+    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
+
+finally:
+    if con:
+        con.close()
